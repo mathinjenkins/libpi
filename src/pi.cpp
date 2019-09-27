bfc8068e2ce21ae125ac5254cfa0f93c71f06686
Marina Hesham <marinahesham21@gmail.com>
2019-09-27 17:36:42 -0400

    Update pi.cpp

--- src/pi.cpp
+++ src/pi.cpp
@@ -4,7 +4,7 @@ float GetPi(int n){
     // Nilakantha Series
     float pi = 3;
     int sign = 1;
-    int start = 2;
+    int start = 2
     for (int i=1; i<n; i++){
         pi += sign * (4.0 / (start * (start + 1) * (start + 2)));
         sign *= -1;
